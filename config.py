f5fdd3cdc4775979da73b73022c40c71ed314e91
norman0402 <103723359+norman0402@users.noreply.github.com>
2022-04-15 13:25:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cs2ei0r3niij.us-east-1.rds.amazonaws.com"
+customuser = "aws_iuser"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "norman-employee"
+customregion = "us-east-1"
