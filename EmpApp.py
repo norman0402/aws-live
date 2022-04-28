d69e2276dd0ad9609db4b3cc8b043822d3205203
norman0402 <103723359+norman0402@users.noreply.github.com>
2022-04-28 16:15:29 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -21,7 +21,7 @@ output = {}
 table = 'employee'
 
 
-@app.route("/index", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 	
